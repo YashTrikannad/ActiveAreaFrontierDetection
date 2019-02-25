4959f9eef0ea4a4a112b000031ba4373cd52d861
Yash Trikannad <yash.trikannad@gmail.com>
2019-02-25 18:49:25 -0500

    Returns Centroids of Frontier Groups

--- 3d_frontiers.cpp
+++ 3d_frontiers.cpp
@@ -1,68 +1,13 @@
+// Yash Trikannad
+// For Pluto (DARPA SubT Challenge UPenn)
+
 #include <iostream>
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
 #include "3d_frontiers.h"
-#define ROW 10
-#define COL 10
-#define DEP 10
 
-bool FrontierDetection::is_Frontier(const int &i, const int &j, const int&k){
-    this->neighbours << this->map_3d(i-1, j, k), this->map_3d(i+1, j, k), this->map_3d(i, j-1, k),
-            this->map_3d(i, j+1, k), this->map_3d(i, j, k-1), this->map_3d(i, j, k+1);
-    return (this->neighbours.array() == 1).any();
-}
-
-// A function to check if a given cell (row, col) can be included in DFS
-int FrontierDetection::isSafe(const Eigen::Tensor<bool, 3> &M,const int &row,const int &col,const int &dep)
-{
-    // row number is in range, column number is in range and value is 1
-    // and not yet visited
-    return (row >= 0) && (row < ROW) &&
-           (col >= 0) && (col < COL) &&
-           (dep >= 0) && (dep < DEP) &&
-           (M(row, col, dep) && !this->visited(row, col, dep));
-}
-
-void FrontierDetection::DFS(const Eigen::Tensor<bool, 3> &M, const int &row, const int &col, const int &dep)
-{
-    // These arrays are used to get row and column numbers of 8 neighbours
-    // of a given cell
-    static int rowNbr[] = {-1, -1, -1, 0, 0, 1, 1, 1, -1, -1, -1, 0, 0, 1, 1, 1, 0, -1, -1, -1,  0, 0,  1, 1, 1, 0};
-    static int colNbr[] = {-1, 0, 1, -1, 1, -1, 0, 1, -1, 0, 1, -1, 1, -1, 0, 1, 0, -1, 0, 1, -1, 1, -1, 0, 1, 0};
-    static int depNbr[] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,-1, -1, -1, -1, -1, -1, -1, -1};
-    // Mark this cell as visited
-    this->visited(row, col, dep) = true;
-
-    // Recur for all connected neighbours
-    for (int k = 0; k < 8; ++k)
-        if (isSafe(M, row + rowNbr[k], col + colNbr[k], dep + depNbr[k]) )
-            DFS(M, row + rowNbr[k], col + colNbr[k],dep + depNbr[k]);
-}
-
-int FrontierDetection::countIslands(const Eigen::Tensor<bool, 3> &M)
-{
-    // Make a bool array to mark visited cells.
-    // Initially all cells are unvisited
-//    Eigen::Tensor<bool, 3> visited(ROW,COL,DEP);
-    this->visited.setZero();
-
-    // Initialize count as 0 and travese through the all cells of
-    // given matrix
-    int count = 0;
-    for (int i = 0; i < ROW; ++i)
-        for (int j = 0; j < COL; ++j)
-            for (int k = 0; k < DEP; ++k)
-            if (M(i, j, k) && !this->visited(i, j, k)) // If a cell with value 1 is not
-            {                         // visited yet, then new island found
-                DFS(M, i, j, k);     // Visit all cells in this island.
-                ++count;                   // and increment island count
-            }
-
-    return count;
-}
-
-
-
+// Function to Find the Frontiers
+// Adds to Queue all new centroids of frontiers
 
 void NaiveActiveAreaFrontierDetection(const Eigen::Tensor<int, 3> &map_3d){
     FrontierDetection fd(map_3d);
@@ -77,13 +22,7 @@ void NaiveActiveAreaFrontierDetection(const Eigen::Tensor<int, 3> &map_3d){
                     if(fd.is_Frontier(i, j, k)){
                         fd.frontier_map(i, j, k) = true;
                     }
-
-//                    if(fd.frontier_map(i, j, k)){
-//                        // Remove Frontier
-//                        fd.frontier_map(i, j, k) = false;
-//                        continue;
-//                    }
-
+                    // Write Condition to remove Frontiers
                 }
                 else{
                     continue;
@@ -92,13 +31,83 @@ void NaiveActiveAreaFrontierDetection(const Eigen::Tensor<int, 3> &map_3d){
             }
         }
     }
-//    Eigen::Tensor<bool, 3> frontier_map;
-    int numberIslands = fd.countIslands(fd.frontier_map);
-    std::cout << numberIslands << std::endl;
+
+    fd.FindFrontierGroups(fd.frontier_map);
 //    std::cout << fd.frontier_map << std::endl;
 //    std::cout << map_3d;
 }
 
+
+// Find Frontier Groups among using the Frontier Map predicted in the previous step
+void FrontierDetection::FindFrontierGroups(const Eigen::Tensor<bool, 3> &frontier_map)
+{
+    // Initialize count as 0 and travese through the all cells of
+    // given matrix
+    int count = 0;
+    meanF mean = {0, 0, 0};
+    for (int i = 0; i < this->dimX; ++i)
+        for (int j = 0; j < this->dimY; ++j)
+            for (int k = 0; k < this->dimZ; ++k)
+                if (frontier_map(i, j, k) && !this->visited(i, j, k)) // If a cell with value 1 is not
+                {
+                    this->resetPrevious();
+                    DFS(frontier_map, i, j, k);// Visit all cells in this island.
+                    ++count;                   // and increment island count
+                    mean.x = this->groupx_mean/this->group_count;
+                    mean.y = this->groupy_mean/this->group_count;
+                    mean.z = this->groupz_mean/this->group_count;
+                    this->frontier_queue.push(mean);
+                    std::cout << "Row: " <<mean.x << " Column: " << mean.y << " Depth: " << mean.z <<std::endl;
+                }
+}
+
+bool FrontierDetection::is_Frontier(const int &i, const int &j, const int&k){
+    this->neighbours << this->map_3d(i-1, j, k), this->map_3d(i+1, j, k), this->map_3d(i, j-1, k),
+            this->map_3d(i, j+1, k), this->map_3d(i, j, k-1), this->map_3d(i, j, k+1);
+    return (this->neighbours.array() == 1).any();
+}
+
+
+// A function to check if a given cell (row, col, dep) can be included in DFS
+int FrontierDetection::isSafe(const Eigen::Tensor<bool, 3> &frontier_map,const int &row,const int &col,const int &dep)
+{
+    // Check for Boundary Conditions
+    return (row >= 0) && (row < this->dimX) &&
+           (col >= 0) && (col < this->dimY) &&
+           (dep >= 0) && (dep < this->dimZ) &&
+           (frontier_map(row, col, dep) && !this->visited(row, col, dep));
+}
+
+// Recursive Function to Mark all Frontier Points for a single group
+void FrontierDetection::DFS(const Eigen::Tensor<bool, 3> &frontier_map, const int &row, const int &col, const int &dep)
+{
+    // These arrays are used to get row and column numbers of 8 neighbours
+    // of a given cell
+    static int rowNbr[] = {-1, -1, -1, 0, 0, 1, 1, 1, -1, -1, -1, 0, 0, 1, 1, 1, 0, -1, -1, -1,  0, 0,  1, 1, 1, 0};
+    static int colNbr[] = {-1, 0, 1, -1, 1, -1, 0, 1, -1, 0, 1, -1, 1, -1, 0, 1, 0, -1, 0, 1, -1, 1, -1, 0, 1, 0};
+    static int depNbr[] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    // Mark this cell as visited
+    this->visited(row, col, dep) = true;
+
+    this->group_map(row, col, dep) = 1;
+    this->group_count += 1;
+    this->groupx_mean += row;
+    this->groupy_mean += col;
+    this->groupz_mean += dep;
+
+    // Recur for all connected neighbours
+    for (int k = 0; k < 26; ++k)
+        if (isSafe(frontier_map, row + rowNbr[k], col + colNbr[k], dep + depNbr[k]) )
+            DFS(frontier_map, row + rowNbr[k], col + colNbr[k],dep + depNbr[k]);
+}
+
+void FrontierDetection::resetPrevious(){
+    this->group_map.setConstant(0);
+    this->groupx_mean = 0;
+    this->groupy_mean = 0;
+    this->groupz_mean = 0;
+    this->group_count = 0;
+}
 
 int main()
 {
--- 3d_frontiers.h
+++ 3d_frontiers.h
@@ -1,32 +1,56 @@
-
 #pragma once
 #include <queue>
 
 class FrontierDetection{
 
 private:
+    struct meanF{
+        double x;
+        double y;
+        double z;
+    };
+    int dimX;
+    int dimY;
+    int dimZ;
     Eigen::Tensor<int, 3> map_3d;
     Eigen::Matrix<int, 6, 1> neighbours;
+    double groupx_mean;
+    double groupy_mean;
+    double groupz_mean;
+    double group_count;
+
 public:
     Eigen::Tensor<bool, 3> frontier_map;
-    std::queue<int[3]> frontier_queue;
+    Eigen::Tensor<int, 3> group_map;
+    std::queue<meanF> frontier_queue;
     Eigen::Tensor<bool, 3> visited;
 
     // Constructor
     explicit FrontierDetection(const Eigen::Tensor<int, 3> &map_In): map_3d(map_In)
     {
         Eigen::Tensor<int, 3>::Dimensions dims = map_3d.dimensions();
-        visited = Eigen::Tensor<bool, 3>(dims[0],dims[1],dims[2]);
-        frontier_map = Eigen::Tensor<bool, 3>(dims[0], dims[1], dims[2]);
+        dimX = int(dims[0]);
+        dimY = int(dims[1]);
+        dimZ = int(dims[2]);
+        visited = Eigen::Tensor<bool, 3>(dimX,dimY,dimZ);
+        visited.setConstant(false);
+        frontier_map = Eigen::Tensor<bool, 3>(dimX,dimY,dimZ);
         frontier_map.setConstant(false);
+        group_map = Eigen::Tensor<int, 3>(dimX,dimY,dimZ);
+        group_map.setConstant(0);
         neighbours.setZero();
         visited.setConstant(false);
+        groupx_mean = 0;
+        groupy_mean = 0;
+        groupz_mean = 0;
+        group_count = 0;
     };
 
     bool is_Frontier(const int &i, const int &j, const int&k);
     int isSafe(const Eigen::Tensor<bool, 3> &,const int &,const int &,const int &);
     void DFS(const Eigen::Tensor<bool, 3> &, const int &, const int &, const int &);
-    int countIslands(const Eigen::Tensor<bool, 3> &);
+    void FindFrontierGroups(const Eigen::Tensor<bool, 3> &);
+    void resetPrevious();
 
 };
 
