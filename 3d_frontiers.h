5385cf8a8c2050df101e5a2b4fc74aef39c17aac
Yash Trikannad <yash.trikannad@gmail.com>
2019-03-02 07:23:09 -0500

    Update 3d_frontiers.h

--- 3d_frontiers.h
+++ 3d_frontiers.h
@@ -1,12 +1,13 @@
 // Yash Trikannad
 // For Pluto (DARPA SubT Challenge UPenn)
+
 #pragma once
 #include <vector>
 
-struct meanF{
+struct meanFrontier{
     double x;
     double y;
     double z;
-};
+    };
 
-std::vector<meanF> NaiveActiveAreaFrontierDetection(const Eigen::Tensor<int8_t , 3> &map_3d);
+std::vector<meanFrontier> NaiveActiveAreaFrontierDetection(const Eigen::Tensor<int8_t , 3> &map_3d);
